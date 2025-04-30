0809148c40974065b1c2a4a8de9df82707c575cd
BladeSabre <bladethecoder@gmail.com>
2025-05-01 00:00:00 +0100

    Fix Witches communication

--- decode_witches.py
+++ decode_witches.py
@@ -2,33 +2,25 @@
 import json
 
 CLOCK = 19520
-DECODE_ADJUST_HIGH = 0
-DECODE_ADJUST_LOW = CLOCK // 2
 
 def decode(pulses):
 	result = []
 	current_byte = 0
 	bit_count = 0
-	level = True
+	level = 1
 	for pulse in pulses:
-		if level:
-			# falling edge, round down prev high
-			new_bits = (pulse + DECODE_ADJUST_HIGH) // CLOCK
-			for _ in range(new_bits):
-				current_byte <<= 1
-				current_byte |= 1
-			bit_count += new_bits
-		else:
-			# rising edge, round up prev low
-			new_bits = (pulse + DECODE_ADJUST_LOW) // CLOCK
-			current_byte <<= new_bits
-			bit_count += new_bits
-		level = not level
+		new_bits = round(pulse / CLOCK)
+		for _ in range(new_bits):
+			current_byte <<= 1
+			current_byte |= level
+		bit_count += new_bits
+		level = 1 - level
 		if bit_count == 10:
 			result.append((current_byte >> 1) & 0xFF)
 			current_byte = 0
 			bit_count = 0
 		if bit_count > 10:
+			# this is an error: raise exception in the real thing
 			result.append(0x1000 + bit_count)
 			return result
 	current_byte <<= 10 - bit_count
@@ -39,7 +31,9 @@ if __name__ == "__main__":
     with open("irdata.json") as f:
         for item in json.load(f)["data"]:
             if item["id"].startswith("mw"):
-                decoding = decode(item["A"][1:])
+                pulses = item["A"][1:]
+                decoding = decode(pulses)
                 print(" ".join("%02X" % x for x in decoding), end="\t")
+                #print(" ".join("%.2f" % (x / CLOCK) for x in pulses), end="\t")
                 print(item["id"])
 
